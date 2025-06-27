b0e2220a585011c9bcaeaebf3be7a19ed5685a03
novalrmdhan9 <153193391+novalrmdhan9@users.noreply.github.com>
2025-06-28 02:29:39 +0700

    read.php

--- banner/read.php
+++ banner/read.php
@@ -1,11 +1,13 @@
-<?php
 include '../db.php';
 header('Content-Type: application/json');
+
+$cloudinary_base_url = "https://res.cloudinary.com/do29rrjxg/image/upload/";
 
 $query = mysqli_query($conn, "SELECT * FROM banner");
 
 $result = [];
 while($row = mysqli_fetch_assoc($query)) {
+    $row['gambar'] = $cloudinary_base_url . $row['gambar'];
     $result[] = $row;
 }
 
@@ -13,4 +15,3 @@ echo json_encode([
     'success' => true,
     'data' => $result
 ]);
-?>
