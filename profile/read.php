9ec762ac619faf406fa127445c0db6e5c098e3c8
novalrmdhan9 <153193391+novalrmdhan9@users.noreply.github.com>
2025-06-28 04:34:34 +0700

    Update read.php

--- profile/read.php
+++ profile/read.php
@@ -2,7 +2,6 @@
 include '../db.php';
 header('Content-Type: application/json');
 
-// Cek apakah ada parameter email
 if (!isset($_GET['email'])) {
     echo json_encode(["success" => false, "message" => "Email tidak ditemukan"]);
     exit;
@@ -10,22 +9,30 @@ if (!isset($_GET['email'])) {
 
 $email = $_GET['email'];
 
-// Query ke database
+// Cloudinary config
+$cloudinary_base = "https://res.cloudinary.com/do29rrjxg/image/upload/";
+
 $query = mysqli_query($conn, "SELECT * FROM profile_users WHERE email='$email'");
 
 if (mysqli_num_rows($query) > 0) {
     $result = mysqli_fetch_assoc($query);
+
+    // Pastikan hanya tampilkan URL Cloudinary jika field avatar tidak kosong
+    $avatarUrl = !empty($result['avatar'])
+        ? $cloudinary_base . $result['avatar']
+        : '';
+
     echo json_encode([
         "success" => true,
         "data" => [
             "alamat" => $result['alamat'],
             "no_hp" => $result['no_hp'],
-            "avatar" => $result['avatar']
+            "avatar" => $avatarUrl
         ]
     ]);
 } else {
     echo json_encode([
-        "success" => true, // Tetep true, tapi data null (biar gampang handle di frontend)
+        "success" => true,
         "data" => null
     ]);
 }
