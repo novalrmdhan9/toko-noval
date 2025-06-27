4ca720f2a90881f91b31e8f6ae1bc77e378de1f8
novalrmdhan9 <153193391+novalrmdhan9@users.noreply.github.com>
2025-06-28 04:59:03 +0700

    Update read.php

--- profile/read.php
+++ profile/read.php
@@ -9,25 +9,17 @@ if (!isset($_GET['email'])) {
 
 $email = $_GET['email'];
 
-// Cloudinary config
-$cloudinary_base = "https://res.cloudinary.com/do29rrjxg/image/upload/";
-
 $query = mysqli_query($conn, "SELECT * FROM profile_users WHERE email='$email'");
 
 if (mysqli_num_rows($query) > 0) {
     $result = mysqli_fetch_assoc($query);
-
-    // Pastikan hanya tampilkan URL Cloudinary jika field avatar tidak kosong
-    $avatarUrl = !empty($result['avatar'])
-        ? $cloudinary_base . $result['avatar']
-        : '';
 
     echo json_encode([
         "success" => true,
         "data" => [
             "alamat" => $result['alamat'],
             "no_hp" => $result['no_hp'],
-            "avatar" => $avatarUrl
+            "avatar" => $result['avatar'] ?? ''
         ]
     ]);
 } else {
