af456b7c010b82b3e7db21da710cec997605eb41
novalrmdhan9 <153193391+novalrmdhan9@users.noreply.github.com>
2025-06-28 17:53:27 +0700

    Update read_transaksi.php

--- read_transaksi.php
+++ read_transaksi.php
@@ -2,25 +2,32 @@
 include '../db.php';
 header('Content-Type: application/json');
 
+// Ambil semua transaksi
 $query = "SELECT * FROM transaksi ORDER BY id_transaksi DESC";
 $result = mysqli_query($conn, $query);
 
 $data = [];
+
 while ($row = mysqli_fetch_assoc($result)) {
     $id = $row['id_transaksi'];
-    $detail = [];
 
-    $detailResult = mysqli_query($conn, "SELECT * FROM transaksi_detail WHERE id_transaksi = $id");
-    while ($d = mysqli_fetch_assoc($detailResult)) {
-        $detail[] = $d;
+    // Ambil detail barang untuk transaksi ini
+    $detailQuery = "SELECT nama_barang, harga, gambar FROM transaksi_detail WHERE id_transaksi = ?";
+    $detailStmt = $conn->prepare($detailQuery);
+    $detailStmt->bind_param("i", $id);
+    $detailStmt->execute();
+    $detailResult = $detailStmt->get_result();
+
+    $items = [];
+    while ($d = $detailResult->fetch_assoc()) {
+        $items[] = $d;
     }
 
-    $row['items'] = $detail;
+    $row['items'] = $items;
     $data[] = $row;
 }
 
 echo json_encode([
-  "success" => true,
-  "data" => $data
+    "success" => true,
+    "data" => $data
 ]);
-?>
